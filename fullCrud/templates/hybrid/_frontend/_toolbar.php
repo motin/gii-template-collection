a8445dc04df7bd3f5a4c8a576e97e861c7ae0c67
Fredrik Wollsén <fredrik@neam.se>
2014-03-04 14:44:16 -0500

    All Tb* includes use \Tb instead of plain Tb or bootstrap.widgets.Tb

--- fullCrud/templates/hybrid/_frontend/_toolbar.php
+++ fullCrud/templates/hybrid/_frontend/_toolbar.php
@@ -4,34 +4,34 @@
         echo '<?php
         switch ($this->action->id) {
             case "index":
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Manage"),
                     "icon" => "icon-edit",
                     "url" => array("admin")
                 ));
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Add"),
                     "icon" => "icon-edit",
                     "url" => array("add")
                 ));
                 break;
             case "view":
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Manage"),
                     "icon" => "icon-edit",
                     "url" => array("admin")
                 ));
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Edit"),
                     "icon" => "icon-edit",
                     "url" => array("continueAuthoring", "id" => $model->{$model->tableSchema->primaryKey})
                 ));
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Update"),
                     "icon" => "icon-edit",
                     "url" => array("update", "id" => $model->{$model->tableSchema->primaryKey})
                 ));
-                $this->widget("bootstrap.widgets.TbButton", array(
+                $this->widget("\TbButton", array(
                     "label" => Yii::t("' . $this->messageCatalog . '", "Delete"),
                     "type" => "danger",
                     "icon" => "icon-remove icon-white",
